20203c83881e6e7731f4e5ec4713320691be3110
Andrew Yuan <andrewyuan.nj@gmail.com>
2025-11-19 17:31:23 -0500

    partially working

--- ps5/bgrep.c
+++ ps5/bgrep.c
@@ -11,6 +11,106 @@
 #include <errno.h>
 #include <stdbool.h>
 
+void scan_file(char *filename, unsigned char *pattern, unsigned long len, int context, bool *fatal_error, bool *global_match){
+
+    int fd;
+    struct stat st;
+    bool sigbus = false;
+
+    // If stdin then report the error
+    if (filename == NULL){
+        fprintf(stderr, "stdin cannot be mmapped\n");
+        *fatal_error = true;
+        return 1;
+    }
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0){
+        fprintf(stderr, "Can't open %s for reading: %s\n", filename, strerror(errno));
+        *fatal_error = true;
+        return 255;
+    }
+
+    if (fstat(fd, &st) < 0){
+        fprintf(stderr, "fstat failed on %s: %s\n", filename, strerror(errno));
+        *fatal_error = true;
+        close(fd);
+        return 255;
+    }
+
+    // If the file is empty
+    if (st.st_size < len){
+        close(fd);
+        return 0;
+    }
+
+    //stackoverflow.com/questions/13642381/c-c-why-to-use-unsigned-char-for-binary-data?
+    unsigned char *addr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED){
+        fprintf(stderr, "mmap failed on %s: %s", filename, strerror(errno));
+        *fatal_error = true;
+        close(fd);
+        return;
+    }
+
+    // SIGBUS protection
+    char *current_file = filename;
+    if (sigsetjmp(env, 1) != 0){
+        // came back from SIGBUS
+        munmap(addr, st.st_size);
+        close(fd);
+        return;
+    }
+
+    // Pattern searching through file
+    for (off_t i = 0; i + len <= st.st_size; i++){
+
+        // First check if SIGBUS occurred
+        if (sigbus) {
+            munmap(addr, st.st_size);
+            close(fd);
+            *fatal_error = true;
+            return;
+        }
+
+        if (memcmp(addr + i, pattern, len) == 0){
+           *global_match = true;
+
+            // compute context range
+            size_t start;
+            if (i >= context){
+                start = i - context;
+            }else{
+                start = 0;
+            }
+            size_t end = i + len + context;
+            if (end > st.st_size){
+                end = st.st_size;
+            }
+            // print filename + offset
+            printf("%s:%ld ", filename, (long)i); // cast into long insead of off_t (refer to previous source down below)
+
+            // ASCII
+            for (size_t j = start; j < end; j++){
+                unsigned char c = addr[j];
+                if (isprint(c))
+                    putchar(c);
+                else
+                    putchar('?');
+            }
+            putchar(' ');
+
+            // HEX
+            for (size_t j = start; j < end; j++){
+                printf("%02x", addr[j]);
+            }
+            putchar('\n');
+        }
+    }
+
+    munmap(addr, st.st_size);
+    close(fd);
+}
 
 int main(int argc, char** argv){
 
@@ -23,7 +123,7 @@ int main(int argc, char** argv){
 
     if (argc == 1) {
         fprintf(stderr, "Error: bgrep arguments not provided\n");
-        return 255;
+        return 1;
     }
 
     while ((opt = getopt(argc, argv, "pc:")) != -1) {
@@ -35,21 +135,97 @@ int main(int argc, char** argv){
 
                 if (optind >= argc) { // requires file input if -p flag
                     fprintf(stderr, "Error: -p requires a pattern file\n"); 
-                    return 255;
+                    return 1;
                 }
 
                 use_pattern = true;
                 pattern_file = argv[optind]; // set pattern_file
                 optind++;   // consume the pattern filename
                 break;
+                
             default:
                 fprintf(stderr, "usage: bgrep [-c N] [-p patternfile] pattern files...\n"); // proper usage of command
-                return 255;
+                return 1;
         }
     }
 
     if (!use_pattern && optind >= argc) { // if no pattern was supplied
         fprintf(stderr, "Error: no search pattern provided\n");
+        return 1;
+    }
+
+    char *pattern = NULL;
+    size_t len = 0; //www.reddit.com/r/C_Programming/comments/e4hro6/when_to_use_size_t/
+
+    // Pattern comes from -p or literal pattern
+    // Case 1: -p provided
+    if(use_pattern == true){
+        int p_fd = open(pattern_file, O_RDONLY);
+        if (p_fd < 0) {
+            fprintf(stderr, "Can't open pattern file %s: %s\n",
+                    pattern_file, strerror(errno));
+            return 255;
+        }
+
+        struct stat p_st;
+        if (fstat(p_fd, &p_st) < 0) {
+            perror("fstat");
+            close(p_fd);
+            return 255;
+        }
+
+        len = p_st.st_size;
+        if (len == 0){
+            fprintf(stderr, "Error: pattern file %s is empty\n", pattern_file);
+            close(p_fd);
+            return 1;
+        }
+
+        pattern = malloc(len);
+        if (pattern == NULL){
+            perror("malloc");
+            close(p_fd);
+            return 255;
+        }
+
+        unsigned long n = read(p_fd, pattern, len);
+        if (n != len){
+            perror("read");
+            close(p_fd);
+            free(pattern);
+            return 255;
+        }
+        close(p_fd);
+
+    // Case 2: Literal Pattern
+    }else{
+        pattern = (unsigned char *)argv[optind]; // WTF here???
+        len = strlen((char*)pattern);
+        optind++;  // consume literal pattern
+    }
+    
+    bool fatal_error = false;
+    bool global_match = false;
+    bool use_stdin = false;
+
+    if (optind >= argc){
+        use_stdin = true;
+    }
+
+    if (use_stdin){
+        scan_file(NULL, pattern, len, context, &fatal_error, &global_match);
+    } else{
+        for (int i = optind; i < argc; i++){
+            scan_file(argv[i], pattern, len, context, &fatal_error, &global_match);
+        }
+    }
+
+    if (fatal_error){
         return 255;
     }
+
+    if (global_match){
+        return 0;
+    }
+    return 1;
 }
