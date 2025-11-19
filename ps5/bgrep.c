ab806143d8849597224ecd598d4be91ebd0efb2e
Andrew <andrew.yuan@cooper.edu>
2025-11-18 20:06:47 -0500

    makefile

---
+++ ps5/bgrep.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+#include <getopt.h>
+#include <ctype.h>
